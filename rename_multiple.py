2aa0051f363fefe3da801b262e11c82631116c3e
Juan Carlo ™ <juancarlo.h.junger@hotmail.com>
2018-08-07 17:19:49 -0300

    Update rename_multiple.py

--- rename_multiple.py
+++ rename_multiple.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
 
 files = os.listdir(".")
 for file in files:
-    os.rename(file, file.replace("-min", "")) #params to rename the file
+    os.rename(file, file.replace("before", "after")) #params to rename the file
