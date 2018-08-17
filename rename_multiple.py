be8ee4fa6fa54f9bd4ad40bf5f9bbedb91b8f0da
Juan Carlo ™ <juancarlo.h.junger@hotmail.com>
2018-08-17 11:29:40 -0300

    Delete rename_multiple.py

--- rename_multiple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import os
-
-files = os.listdir(".")
-for file in files:
-    os.rename(file, file.replace("before", "after")) #params to rename the file
