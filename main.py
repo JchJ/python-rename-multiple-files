1c8cf96ffecc2c4c805fe5cd45a1b11f7d778487
Juan Carlo ™ <juancarlo.h.junger@hotmail.com>
2018-08-17 11:29:54 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+files = os.listdir(".")
+for file in files:
+	os.rename(file, file.replace("before", "after")) #params to rename the file
+
